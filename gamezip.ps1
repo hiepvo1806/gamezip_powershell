8709b818498541b6e9a86d97fa057851f93f37a2
hiepvo-groover <hiep.vo@groovetechnology.com>
2018-03-22 12:19:11 +0700

    Init comit

---
+++ gamezip.ps1
@@ -0,0 +1,16 @@
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+Add-Type -Assembly System.IO.Compression.FileSystem
+$folderName = "testfold"
+
+
+$currentDate = Get-Date
+Write-Output $currentDate.ToString("dd-MMM-yyyy-HH-mm-ss")
+$dateString = $currentDate.ToString("dd-MMM-yyyy-HH-mm-ss")
+$compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+$invocation = (Get-Variable MyInvocation).Value
+$directorypath = Split-Path $invocation.MyCommand.Path
+Write-Output $directorypath
+$sourcedir = $directorypath + "\"
+$zipsource = $sourcedir + $folderName
+$zipfilename = $sourcedir + $folderName + "_" + $dateString + ".zip"
+Compress-Archive -Path $zipsource -DestinationPath $zipfilename
